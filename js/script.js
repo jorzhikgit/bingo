e14f32e134167806559b3983c4eb0d5a9de5b52c
Adrian K. Eriksen <adrian@adrianke.com>
2014-10-10 23:03:06 +0200

    Utkast til trekkingsside

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+	$(".noJs").hide();
+
+	var forrigeTall = 0;
+	var antallTrekt = 0;
+
+	$("#trekk").click(function (e) {
+		e.preventDefault();
+
+		$.getJSON("json.php?valg=trekk&forrige=" + forrigeTall, function (data) {
+			if (data.status) {
+				for (var i = 0; i < data.tallene.length; i++) {
+					var tall = data.tallene[i];
+					forrigeTall = tall;
+					nyttTall(tall);
+				}
+			}
+		});
+	});
+
+	$("#kontroller").click(function (e) {
+		e.preventDefault();
+
+		var kontrollnr = $("#kontrollnr").val();
+		$.getJSON("json.php?valg=kontroll&kontrollnr=" + kontrollnr, function (data) {
+			if (data.status) {
+				$("#sjekk").html(data.html).prepend("<p>Vinnertall: " + 
+					data.vinnertall + "</p>");
+			}
+		});
+	});
+
+	function nyttTall(tall) {
+		$("#tall-" + tall).text(tall);
+		$("#talletEr").text(tall);
+
+		var separator = ($("#trekt").text() == "") ? "" : ", ";
+		$("#trekt").prepend(tall + separator);
+
+		antallTrekt++;
+		$("#antall").text(antallTrekt);
+	}
+});
