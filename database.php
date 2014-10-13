2f220f470b80bd72c921fd1c1111da0cc9297ac9
Adrian K. Eriksen <adrian@adrianke.com>
2014-10-13 22:12:38 +0200

    Made a PDO exception, somehow…

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
 
-$db = new PDO('mysql:host=localhost;dbname=bingo', "bingo", "bingoradioen",
+$db = new PDO('mysql:host=127.0.0.1;dbname=bingo', "bingo", "bingoradioen",
               array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 ?>
